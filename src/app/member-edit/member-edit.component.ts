9fd4630aaa92ec3e9934964697ffc9f2a14dc505
Andrew <expandrew@gmail.com>
2017-07-14 19:58:13 -0700

    make some minor tweaks to polish the project.

--- src/app/member-edit/member-edit.component.ts
+++ src/app/member-edit/member-edit.component.ts
@@ -1,6 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { MemberService } from '../member.service';
-// import { Member } from '../member.model';
 
 @Component({
   selector: 'app-edit-member',
